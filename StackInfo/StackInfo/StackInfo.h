f0ca666948272a4ba08319405403fa027dfc6c86
刘聪 <liucong5@kingsoft.com>
2022-05-12 22:26:50 +0800

    StackInfo 简单完成

--- StackInfo/StackInfo/StackInfo.h
+++ StackInfo/StackInfo/StackInfo.h
@@ -1,8 +1,42 @@
 #pragma once
 
-class StackInfo
+#include <stack>
+#include <string>
+#include <assert.h>
+
+struct StackItem
 {
-
-
+	void* valPtr{ nullptr };
+	std::string typeStr;
 };
 
+class StackInfo
+{
+	std::stack<StackItem> m_stack;
+
+public:
+
+	template<typename T>
+	void push(const T& val) {
+		StackItem item;
+		item.typeStr = typeid(T).name();
+		T* ptr = new T();
+		*ptr = val;
+		item.valPtr = (void*)ptr;
+		m_stack.push(item);
+	}
+
+	template<typename T>
+	void pop(T& val) {
+		StackItem item = m_stack.top();
+		if (std::string(typeid(T).name()) != item.typeStr)
+		{
+			assert(false);
+			return;
+		}
+		T* ptr = (T*)(item.valPtr);
+		val = *ptr;
+		delete item.valPtr;
+		m_stack.pop();
+	}
+};
