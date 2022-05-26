dd10fead77790678c5995702d391f0727ee53fcb
刘聪 <liucong5@kingsoft.com>
2022-05-26 09:54:55 +0800

    修改Cmdline项目结构

--- Cmdline/Cmdline/Command.h
+++ Cmdline/Cmdline/Command.h
@@ -7,7 +7,7 @@ class Command
 {
 public:
 	Command();
-	void option(char _ShortName, const std::string& _Name, const std::string& _Desc, std::function<void()> _CallBackFunc);
+	Command& option(char _ShortName, const std::string& _Name, const std::string& _Desc, std::function<void()> _CallBackFunc);
 	void parse(int argc, char** argv);
 private:
 	std::map<Option_Base, std::function<void()>> m_Options;
